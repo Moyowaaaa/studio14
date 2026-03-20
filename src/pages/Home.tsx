be1567bad78ecd4c1b98ac57e78c94cebe151da5
Moyowaaaa <moyowa7@icloud.com>
2026-03-20 11:06:39 +0100

    feat: readme documentation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect, useRef } from "react";
 import HeroSection from "../components/sections/hero-section";
 import TitleSection from "../components/sections/title-section";
 import Navbar from "../components/layout/nav";
